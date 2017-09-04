c2b135b1549d8f17012efea26ade3f4148165364
Vincent Yan <vincentyan1@gmail.com>
2017-09-04 02:09:56 -0700

    Beautified content files

--- README.md
+++
@@ -1,4 +0,0 @@
-## Project Overview
-This is a personal project from the FreeCodeCamp course. The user can set a work time and break time to however many minutes he or she wants. This app is great for increasing productivity since the pomodoro method has proven to help many others study effectively!
-
-Click <a href="https://envincebal.github.io/pomodoro-clock/">HERE</a> to view app. If you downloaded this to your local machine, you can run the app by opening up the **index.html** file.
--- js/app.js
+++ js/app.js
@@ -7,33 +7,33 @@ var isCounting = false;
 var workTime;
 var breakTime;
 
-document.querySelector(".work-minus").addEventListener("click", function(){
+document.querySelector(".work-minus").addEventListener("click", function () {
 
-  if (!isCounting && initialWork > 1){
+  if (!isCounting && initialWork > 1) {
     initialWork--
     setWork.textContent = initialWork;
     counter.textContent = initialWork;
   }
 });
 
-document.querySelector(".work-plus").addEventListener("click", function(){
-  
-	if (!isCounting) {
-	  initialWork++;
-	  setWork.textContent = initialWork;
-	  counter.textContent = initialWork;
+document.querySelector(".work-plus").addEventListener("click", function () {
+
+  if (!isCounting) {
+    initialWork++;
+    setWork.textContent = initialWork;
+    counter.textContent = initialWork;
   }
 });
 
-document.querySelector(".break-minus").addEventListener("click", function(){
+document.querySelector(".break-minus").addEventListener("click", function () {
 
- 	if (!isCounting && initialBreak > 1){
+  if (!isCounting && initialBreak > 1) {
     initialBreak--
     setBreak.textContent = initialBreak;
-  } 
+  }
 });
 
-document.querySelector(".break-plus").addEventListener("click", function(){
+document.querySelector(".break-plus").addEventListener("click", function () {
 
   if (!isCounting) {
     initialBreak++;
@@ -41,15 +41,15 @@ document.querySelector(".break-plus").addEventListener("click", function(){
   }
 });
 
-document.querySelector(".clock-circle").addEventListener("click", function(){
-	
-  if(!isCounting){
+document.querySelector(".clock-circle").addEventListener("click", function () {
+
+  if (!isCounting) {
     workSession(counter.textContent);
-    isCounting = true;  
-  } 
+    isCounting = true;
+  }
 });
 
-document.querySelector(".reset-circle").addEventListener("click", function(){
+document.querySelector(".reset-circle").addEventListener("click", function () {
   document.querySelector(".status").textContent = "Session";
   document.querySelector(".clock-circle").style.backgroundColor = "#06032a";
   setWork.textContent = initialWork;
@@ -66,24 +66,24 @@ function workSession(minutes) {
   var status = document.querySelector(".status");
   var clock = document.querySelector(".clock-circle");
   var chime = new Audio("http://www.wavlist.com/soundfx/014/cricket-1.wav");
-  
+
   function workTick() {
-    
-    var current_minutes = mins-1;
-    
+
+    var current_minutes = mins - 1;
+
     clock.style.backgroundColor = "#f43a3a";
     status.textContent = "Session";
     seconds--;
     counter.textContent = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-    
-    if(seconds >= 0) {
-      workTime = setTimeout(workTick, 1000);  
-        
-    } else if(mins >= 1){
-      workSession(mins-1);           
+
+    if (seconds >= 0) {
+      workTime = setTimeout(workTick, 1000);
+
+    } else if (mins >= 1) {
+      workSession(mins - 1);
     }
-    
-    if (counter.textContent === "0:00"){
+
+    if (counter.textContent === "0:00") {
       chime.play();
       counter.textContent = initialBreak;
       breakSession(counter.textContent);
@@ -98,22 +98,22 @@ function breakSession(minutes) {
   var status = document.querySelector(".status");
   var clock = document.querySelector(".clock-circle");
   var rooster = new Audio("http://www.wavlist.com/soundfx/009/rooster-2.wav");
-  
+
   function breakTick() {
-    var current_minutes = mins-1;
-    
+    var current_minutes = mins - 1;
+
     clock.style.backgroundColor = "#0daa32";
     status.textContent = "Break";
     seconds--;
     counter.textContent = current_minutes.toString() + ":" + (seconds < 10 ? "00" : "") + String(seconds);
-    
-    if(seconds >= 0) {
+
+    if (seconds >= 0) {
       breakTime = setTimeout(breakTick, 1000);
-    } else if(mins > 1){
-      breakSession(mins-1);           
+    } else if (mins > 1) {
+      breakSession(mins - 1);
     }
-    
-    if (counter.textContent === "0:00"){
+
+    if (counter.textContent === "0:00") {
       rooster.play();
       counter.textContent = initialWork;
       workSession(counter.textContent);
